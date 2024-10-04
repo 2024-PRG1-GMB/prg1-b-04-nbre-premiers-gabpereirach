794bbbd16ef013190793bb9ccf117751db11ef81
gabpereira <gabrielpereira.gpch@gmail.com>
2024-10-04 14:43:44 +0200

    Add basic main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * \file main.cpp
+ * \version 1.0.0
+ * \author Pereira Gabriel
+ * \date : 04.10.2024
+ * \brief : Affiche tous les nombres premiers de 1 jusqu'au chiffre entré
+ */
+ 
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() 
+{
+    cout << "Ce programme affiche tous les nombres premiers de 1 jusqu'au chiffre entré par l'utilisatuer." << endl;
+
+
+	return EXIT_SUCCESS;
+}
