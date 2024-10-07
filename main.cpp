bd54070c9d6fa8e56e41b77930a3b8f4ccae15e1
gabpereira <gabrielpereira.gpch@gmail.com>
2024-10-07 08:52:03 +0200

    Update the labo

--- main.cpp
+++ main.cpp
@@ -5,15 +5,45 @@
  * \date : 04.10.2024
  * \brief : Affiche tous les nombres premiers de 1 jusqu'au chiffre entré
  */
- 
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
-int main() 
-{
+int main() {
     cout << "Ce programme affiche tous les nombres premiers de 1 jusqu'au chiffre entré par l'utilisatuer." << endl;
 
 
-	return EXIT_SUCCESS;
+    char choix;
+    do {
+        int nombre_saisie;
+        do {
+            cout << "entrer une valeur [2-1000] : ";
+            cin >> nombre_saisie;
+            cout << endl;
+        } while (nombre_saisie < 2 || nombre_saisie > 1000);
+
+        cout << "Voici la liste des nombres premiers" << endl;
+        for (int auMax = 2; auMax <= nombre_saisie; ++auMax) {
+            int comptage = 0;
+            for (int diviseur = 2; diviseur < auMax; ++diviseur) {
+                if (auMax % diviseur == 0) {
+                    ++comptage;
+                }
+            }
+
+            if (comptage == 0) {
+                cout << auMax << " ";
+            }
+        }
+        cout << endl;
+
+        do {
+            cout << "Voulez-vous recommencer [O/N] : ";
+            cin >> choix;
+            cout << endl;
+        } while (choix != 'O' && choix != 'N');
+    } while (choix != 'N');
+
+    cout << "Fin de programme" << endl;
+    return EXIT_SUCCESS;
 }
