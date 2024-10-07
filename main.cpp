14d539845af778c9b8d18d226f022584257d565c
gabpereira <gabrielpereira.gpch@gmail.com>
2024-10-07 09:39:32 +0200

    Update name of variables

--- main.cpp
+++ main.cpp
@@ -12,35 +12,35 @@ using namespace std;
 int main() {
     cout << "Ce programme affiche tous les nombres premiers de 2 jusqu'au chiffre entré par l'utilisatuer." << endl;
 
-    char choix;
+    char retry;
     do {
-        int nombre_saisie;
+        int number_enter;
         do {
             cout << "entrer une valeur [2-1000] : ";
-            cin >> nombre_saisie;
+            cin >> number_enter;
             cout << endl;
-        } while (nombre_saisie < 2 || nombre_saisie > 1000);
+        } while (number_enter < 2 || number_enter > 1000);
 
         cout << "Voici la liste des nombres premiers" << endl;
-        for (int auMax = 2; auMax <= nombre_saisie; ++auMax) {
-            int comptage = 0;
-            for (int diviseur = 2; diviseur < auMax; ++diviseur) {
-                if (auMax % diviseur == 0) {
-                    ++comptage;
+        for (int number_tested = 2; number_tested <= number_enter; ++number_tested) {
+            int counter = 0;
+            for (int divider = 2; divider < number_tested; ++divider) {
+                if (number_tested % divider == 0) {
+                    ++counter;
                 }
             }
-            if (comptage == 0) {
-                cout << '\t' << auMax << " ";
+            if (counter == 0) {
+                cout << '\t' << number_tested << " ";
             }
         }
         cout << endl;
 
         do {
             cout << "Voulez-vous recommencer [O/N] : ";
-            cin >> choix;
+            cin >> retry;
             cout << endl;
-        } while (choix != 'O' && choix != 'N');
-    } while (choix != 'N');
+        } while (retry != 'O' && retry != 'N');
+    } while (retry != 'N');
 
     cout << "Fin de programme" << endl;
     return EXIT_SUCCESS;
