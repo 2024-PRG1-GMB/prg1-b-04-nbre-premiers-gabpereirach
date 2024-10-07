da57ded28ad5f02e9e244bf1d767ed86a4d0c144
gabpereira <gabrielpereira.gpch@gmail.com>
2024-10-07 09:16:54 +0200

    Correct text

--- main.cpp
+++ main.cpp
@@ -3,15 +3,14 @@
  * \version 1.0.0
  * \author Pereira Gabriel
  * \date : 04.10.2024
- * \brief : Affiche tous les nombres premiers de 1 jusqu'au chiffre entré
+ * \brief : Affiche tous les nombres premiers de 2 jusqu'au chiffre entré
  */
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
 int main() {
-    cout << "Ce programme affiche tous les nombres premiers de 1 jusqu'au chiffre entré par l'utilisatuer." << endl;
-
+    cout << "Ce programme affiche tous les nombres premiers de 2 jusqu'au chiffre entré par l'utilisatuer." << endl;
 
     char choix;
     do {
@@ -30,7 +29,6 @@ int main() {
                     ++comptage;
                 }
             }
-
             if (comptage == 0) {
                 cout << auMax << " ";
             }
